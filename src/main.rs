c3a5eb1113f4fa97cf30ad97b37823169a21e265
Jayanta Pradhan <pradhanjayanta91@gmail.com>
2026-01-11 19:32:55 +0530

    impliment a REPL

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,18 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        // Wait for user input
+        let mut command = String::new();
+        io::stdin().read_line(&mut command);
+
+        let command = command.trim();
+        if command.is_empty() {
+            continue;
+        }
+        println!("{}: command not found", command.trim());
+    }
 }
